18fd44420e455ee0140fcde5ec4a73235cbaa4f7
Daniel <emmanaorg@gmail.com>
2024-01-11 01:37:22 -0600

    fix: :bug: condicional para prevenir que la clase Facade este disponible antes de intentar usar aliases.php
    A veces olvido el orden de carga de loader y aliases , para eso la restriccion

--- aliases.php
+++ aliases.php
@@ -2,6 +2,14 @@
 
 use DgbAuroCore\vendor\Inventarium\Facade;
 
+$clase = 'DgbAuroCore\vendor\Inventarium\Facade';
+
+// Comprobar si la clase está disponible
+if (!class_exists($clase)) {
+    // La clase está disponible, puedes utilizarla
+    throw new \Exception("aliases Err: No se encuentra la clase Facade, primero debes requerir el loader");
+}
+
 //CORE
 Facade::addAlias('Cnx', 'DgbAuroCore\vendor\Inventarium\Cnx');
 Facade::addAlias('DgbAuroCore\vendor\Inventarium\interfaces\SessionManagerInterface', 'DgbAuroCore\vendor\Inventarium\Sentinela');
@@ -17,3 +25,4 @@ Facade::addAlias('EventSender', 'DgbAuroCore\vendor\Inventarium\tests\mockups\Ev
 Facade::addAlias('EventSuscriptor', 'DgbAuroCore\vendor\Inventarium\tests\mockups\EventSuscriptor');
 Facade::addAlias('EventSuscriptorTwo', 'DgbAuroCore\vendor\Inventarium\tests\mockups\EventSuscriptorTwo');
 Facade::addAlias('HookSubject', 'DgbAuroCore\vendor\Inventarium\tests\mockups\HookSubject');
+Facade::addAlias('Env', 'DgbAuroCore\vendor\Inventarium\tests\mockups\EnvMockup');
