deaf02d9d0fe63e055daa8733ffc03ef472cd104
Daniel <emmanaorg@gmail.com>
2024-01-13 21:00:33 -0600

    chore: Requiere el parametro version al crear el dist y crea el version.txt en consecuencia en la carpeta /app

--- app/ConsoleComponent/Command/DeployPluginCommand.php
+++ app/ConsoleComponent/Command/DeployPluginCommand.php
@@ -18,12 +18,14 @@ class DeployPluginCommand extends Command
         $this->setName('dagoba:DeployPlugin')
             ->setDescription('Zip del plugin y copia al entorno de producción local')
             ->addArgument('folder', InputArgument::REQUIRED, 'Folder del plugin en desarrollo', null)
+            ->addArgument('version', InputArgument::REQUIRED, 'Version del plugin en desarrollo', null)
             ->addArgument('deleteZip', InputArgument::OPTIONAL, 'Eliminar el zip al finalizar', false);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $folderName = $input->getArgument('folder');
+        $version = $input->getArgument('version');
         $deleteZipOption = $input->getArgument('deleteZip');
         $root = 'C:/laragon/www';
         $folder = 'C:/laragon/www/apps/' . $folderName;
@@ -38,7 +40,7 @@ class DeployPluginCommand extends Command
 
         $zip = $this->createZip($path);
 
-        $zip = $this->addAppFilesToZip($folder, $folderName, $zip);
+        $zip = $this->addAppFilesToZip($folder, $folderName, $zip,$version);
 
         $zip = $this->addDependencyFilesToZip($root, $config, $folderName, $zip);
 
@@ -55,7 +57,7 @@ class DeployPluginCommand extends Command
     }
 
 
-    private function addAppFilesToZip($folder, $folderName, $zip)
+    private function addAppFilesToZip($folder, $folderName, $zip,$version)
     {
 
         // Agregar archivos del folder del parámetro
@@ -98,10 +100,43 @@ class DeployPluginCommand extends Command
             }
         }
 
+        $versionFile = $this->createVersioFile($folder,$version);
+        $zip->addFile($versionFile, "$folderName/app/version.txt");
+
 
         return $zip;
     }
 
+    private function createVersioFile($folder, $version)
+    {
+
+        $versionFile = "$folder/dist/version.txt";
+
+        $maxAttempts = 5;
+        $attempts = 0;
+
+        do {
+            file_put_contents($versionFile, $version);
+            sleep(1);  // Puedes ajustar la pausa según tus necesidades
+            $attempts++;
+        } while (!file_exists($versionFile) && $attempts < $maxAttempts );
+
+        // while ($attempts < $maxAttempts) {
+        //     if (!file_exists($versionFile)) {
+        //         file_put_contents($versionFile, $version);
+        //         break;  // Salir del bucle una vez que se haya creado el versionFile
+        //     } else {
+        //         // El versionFile ya existe, esperar antes de volver a intentar
+        //         $attempts++;
+        //         sleep(2);  // Puedes ajustar la pausa según tus necesidades
+        //     }
+        // }
+
+        if ($attempts === $maxAttempts) {
+            echo "No se pudo crear el versionFile después de $maxAttempts intentos.";
+        }
+        return $versionFile;
+    }
 
     private function checkConfigFile($folder)
     {
@@ -109,7 +144,7 @@ class DeployPluginCommand extends Command
         $configFile = "$folder/dist/dist.config";
         if (!file_exists($configFile)) {
             die('El archivo dist.confog no existe');
-        } 
+        }
         $config = parse_ini_file($configFile, true);
 
         if ($config === false) {
@@ -149,7 +184,7 @@ class DeployPluginCommand extends Command
                         || strpos($file, '.sql') !== false
                         || strpos($file, '.log') !== false
                         || strpos($file, '.zip') !== false
-                        
+
 
                     ) {
                         continue;
@@ -166,7 +201,7 @@ class DeployPluginCommand extends Command
                     }
 
 
-                    $localPath = $folderName . '/vendor/' . $dependencyName. '/' . str_replace($dependencyPath . '/', '', $file);
+                    $localPath = $folderName . '/vendor/' . $dependencyName . '/' . str_replace($dependencyPath . '/', '', $file);
 
                     if (is_dir($file) === true) {
                         // No hacer nada para las carpetas, ya que serán creadas automáticamente durante la extracción
